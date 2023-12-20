304b7db7ed55cd3c7de268f145567e5e7627ee7c
Vasyl Protsukovych <protcykovich@gmail.com>
2023-12-20 20:55:04 +0100

    Added KeyboardInterrupt catch for IMITATE_FULL_CYCLE choice option and possibility to set tickets qty for generation

--- task_1/service_center_imitation.py
+++ task_1/service_center_imitation.py
@@ -1,18 +1,28 @@
 import queue
 import time
 import random
+import uuid
 from enums.command import Command
 
-ticket_types = ["TV repair", "Smartphone repair", "Software upgrade/fix"]
+ticket_types = [
+    "TV repair",
+    "Smartphone repair",
+    "Software upgrade/fix",
+    "Vacuum cleaner repair",
+    "Laptop repair",
+]
 ticket_queue = queue.Queue()
 
 
 def generate_request():
-    # Unique ID imitation
-    request_id = len(ticket_queue.queue) + 1
+    # Unique ID
+    request_id = str(uuid.uuid4())
     # Random ticket type
     request_type = random.choice(ticket_types)
     request = {"ID": request_id, "Type": request_type}
+    print(f"Generating Ticket of type '{request_type}' ...")
+    # Imitate ticket generation time
+    time.sleep(random.uniform(1, 3))
     ticket_queue.put(request)
     print(f"Ticket {request_id} of type '{request_type}' was added to queue.")
 
@@ -28,6 +38,23 @@ def process_request():
         print("Queue is empy.")
 
 
+def get_ticket_qty():
+    while True:
+        tickets_qty = input("Type how many tickets you want to generate: ")
+
+        if tickets_qty.isdigit():  # Перевірка, що введено число
+            return int(tickets_qty)
+        else:
+            print("Please enter a valid number.")
+
+
+def generate_tickets(tickets_qty=None):
+    tickets_qty = tickets_qty or get_ticket_qty()
+    for _ in range(tickets_qty):
+        generate_request()
+    print("All tickets are generated.")
+
+
 while True:
     print("\Interface:")
     print(f"{Command.CREATE_TICKET.value}. Create new ticket")
@@ -39,7 +66,7 @@ while True:
     choice = input("Choose option: ")
 
     if choice == Command.CREATE_TICKET.value:
-        generate_request()
+        generate_tickets()
     elif choice == Command.HANDLE_NEXT_TICKET.value:
         process_request()
     elif choice == Command.HANDLE_ALL_TICKETS.value:
@@ -50,8 +77,17 @@ while True:
                 process_request()
                 print("All tickets were handled.")
     elif choice == Command.IMITATE_FULL_CYCLE.value:
-        generate_request()
-        process_request()
+        tickets_qty = get_ticket_qty()
+
+        try:
+            generate_tickets(tickets_qty)
+
+            print("Starting to process tickets.")
+
+            for _ in range(tickets_qty):
+                process_request()
+        except KeyboardInterrupt:
+            print("\nInterrupted by user.")
     elif choice == Command.QUIT.value:
         print("Programm has finished current session. Have a good day :)")
         break
